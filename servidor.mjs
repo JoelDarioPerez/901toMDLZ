4e9334fc80cfc51d789a49f080a12c07d37c6b3f
Joel Perez <perezjoeldario@gmail.com>
2023-10-05 17:06:16 -0300

    a

--- servidor.mjs
+++ servidor.mjs
@@ -1,19 +1,95 @@
-import dgram from "dgram";
-const server = dgram.createSocket("udp4");
+var udp = require("dgram");
 
-server.on("error", (err) => {
-  console.error(`server error:\n${err.stack}`);
+// --------------------creating a udp server --------------------
+
+// creating a udp server
+var server = udp.createSocket("udp4");
+
+// emits when any error occurs
+server.on("error", function (error) {
+  console.log("Error: " + error);
   server.close();
 });
 
-server.on("message", (msg, rinfo) => {
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+// emits on new datagram msg
+server.on("message", function (msg, info) {
+  console.log("Data received from client : " + msg.toString());
+  console.log(
+    "Received %d bytes from %s:%d\n",
+    msg.length,
+    info.address,
+    info.port
+  );
+
+  //sending msg
+  server.send(msg, info.port, "localhost", function (error) {
+    if (error) {
+      client.close();
+    } else {
+      console.log("Data sent !!!");
+    }
+  });
 });
 
-server.on("listening", () => {
-  const address = server.address();
-  console.log(`server listening ${address.address}:${address.port}`);
+//emits when socket is ready and listening for datagram msgs
+server.on("listening", function () {
+  var address = server.address();
+  var port = address.port;
+  var family = address.family;
+  var ipaddr = address.address;
+  console.log("Server is listening at port" + port);
+  console.log("Server ip :" + ipaddr);
+  console.log("Server is IP4/IP6 : " + family);
 });
 
-server.bind(41234);
-// Prints: server listening 0.0.0.0:41234
+//emits after the socket is closed using socket.close();
+server.on("close", function () {
+  console.log("Socket is closed !");
+});
+
+server.bind(2222);
+
+setTimeout(function () {
+  server.close();
+}, 8000);
+
+// -------------------- udp client ----------------
+
+var buffer = require("buffer");
+
+// creating a client socket
+var client = udp.createSocket("udp4");
+
+//buffer msg
+var data = Buffer.from("siddheshrane");
+
+client.on("message", function (msg, info) {
+  console.log("Data received from server : " + msg.toString());
+  console.log(
+    "Received %d bytes from %s:%d\n",
+    msg.length,
+    info.address,
+    info.port
+  );
+});
+
+//sending msg
+client.send(data, 2222, "localhost", function (error) {
+  if (error) {
+    client.close();
+  } else {
+    console.log("Data sent !!!");
+  }
+});
+
+var data1 = Buffer.from("hello");
+var data2 = Buffer.from("world");
+
+//sending multiple msg
+client.send([data1, data2], 2222, "localhost", function (error) {
+  if (error) {
+    client.close();
+  } else {
+    console.log("Data sent !!!");
+  }
+});
