3109ff3f818daddfc4719f965b3f9af1a1847047
Joel Perez <perezjoeldario@gmail.com>
2023-10-05 11:34:58 -0300

    a

--- servidor.mjs
+++ servidor.mjs
@@ -1,28 +1,24 @@
-import netcat from "node-netcat";
+var NetcatUdpServer = require("node-netcat").udpServer;
+var server = NetcatUdpServer(5000, "127.0.0.1");
 
-// Configurar el servidor UDP
-const port = "6002";
-const server = netcat.udpServer(port); // Puerto en el que el servidor escuchará
-
-// Manejar eventos cuando el servidor esté listo y cuando reciba datos
-server.on("ready", () => {
-  console.log("Servidor UDP listo para recibir datos en el puerto" + port);
+server.on("data", function (msg, client, protocol) {
+  console.log("rx: " + msg + ", from " + client);
 });
 
-server.on("data", (msg, clientInfo) => {
-  console.log(
-    `Mensaje recibido desde ${clientInfo.address}:${clientInfo.port}: ${msg}`
-  );
+server.on("ready", function () {
+  console.log("ready");
 });
 
-// Manejar eventos de error y cierre del servidor
-server.on("error", (err) => {
-  console.error("Error en el servidor UDP:", err);
+server.once("error", function (err) {
+  console.log(err);
 });
 
-server.on("close", () => {
-  console.log("Servidor UDP cerrado.");
+server.once("close", function () {
+  console.log("close");
 });
 
-// Iniciar el servidor UDP
 server.bind();
+
+setTimeout(function () {
+  server.close();
+}, 30000);
