ba4b173858237d302f408dd6bbb42b820d61fe1b
Joel Perez <perezjoeldario@gmail.com>
2023-10-08 18:30:57 -0300

    a

--- handler.mjs
+++ handler.mjs
@@ -2,10 +2,10 @@ import { autoleaders } from "./protocolos.mjs";
 import { AL900 } from "./protocolos.mjs";
 
 export const handler = (data) => {
-  if (data.toString("hex").startsWith("2929") && data.length === 90) {
-    return AL900(data);
-  } else data.startsWith("*HQ");
-  {
+  if (data.startsWith("*HQ")) {
     return autoleaders(data);
+  } else data.toString("hex").startsWith("2929") && data.length === 90;
+  {
+    return AL900(data);
   }
 };
