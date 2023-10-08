e64a7c870571b1ec02221e7a15f7d269ab2099e2
Joel Perez <perezjoeldario@gmail.com>
2023-10-08 18:57:18 -0300

    a

--- handler.mjs
+++ handler.mjs
@@ -1,6 +1,6 @@
 import { autoleaders } from "./protocolos.mjs";
-import { AL900 } from "./protocolos.mjs";
-
+/* import { AL900 } from "./protocolos.mjs";
+ */
 export const handler = (data) => {
   if (data.startsWith("*HQ")) {
     return autoleaders(data);
