6e2d44520ac8866b30eaafa7f1adb628ebb5f76e
Joel Perez <perezjoeldario@gmail.com>
2023-10-08 16:50:00 -0300

    a

--- handler.mjs
+++ handler.mjs
@@ -4,7 +4,7 @@ import { AL900 } from "./protocolos.mjs";
 export const handler = (data) => {
   if (data.startsWith("2929") && data.length === 90) {
     return AL900(data);
-  } else data.startsWith("*HQ") && data.length === 80;
+  } else data.startsWith("*HQ");
   {
     return autoleaders(data);
   }
