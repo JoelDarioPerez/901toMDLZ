7e889f7aac5687cf587fca6536adeeb6479fba79
Joel Perez <perezjoeldario@gmail.com>
2023-10-08 21:34:58 -0300

    a

--- handler.mjs
+++ handler.mjs
@@ -1,15 +1,12 @@
 import { autoleaders } from "./protocolos.mjs";
-/* import { AL900 } from "./protocolos.mjs";
- */
+import { AL900 } from "./protocolos.mjs";
+
 export const handler = (data) => {
   try {
     if (Buffer.byteLength(data) === 99 || Buffer.byteLength(data) === 97) {
       return autoleaders(data);
-    } else {
-      // Manejar otro caso si es necesario
-      console.log("Tamaño de paquete no válido");
-      return null; // Otra acción o valor de retorno si es necesario
-    }
+    } else Buffer.byteLength(data) === 45;
+    return AL900(data);
   } catch (e) {
     console.log(e);
     return null; // Manejo de errores
